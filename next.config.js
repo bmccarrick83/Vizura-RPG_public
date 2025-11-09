4b14e04b7b7f4a3f359bda23cb97da2e673676e6
Brandon Edward Mccarrick <bmccarrick83@users.noreply.github.com>
2025-11-09 13:12:48 +0300

    Add Next.js configuration with TypeScript and ESLint

---
+++ next.config.js
@@ -0,0 +1,57 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/firebasestorage/:path*',
+        destination: 'https://firebasestorage.googleapis.com/:path*',
+      },
+    ]
+  },
+};
+
+export default nextConfig;
+
+    
